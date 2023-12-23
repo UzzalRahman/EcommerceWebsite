d1c04b2a9721b389dbc6c3dc704baaf00c6046d7
Uzzal Rahman <uzzal.ewu95@gmail.com>
2023-12-23 15:46:07 +0600

    Home page implemented.added paging sorting and filtering functionality.

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -5,12 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { CoreModule } from './core/core.module';
+import { ShopModule } from './shop/shop.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavBarComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
@@ -18,6 +18,8 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     BrowserAnimationsModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Client/src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { PagerComponent } from './pager/pager.component';
+import { PagingHeaderComponent } from './paging-header/paging-header.component';
+
+
+@NgModule({
+  declarations: [
+    PagerComponent,
+    PagingHeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    PaginationModule.forRoot()
+  ],
+  exports:[
+    PaginationModule,
+    PagerComponent,
+    PagingHeaderComponent
+  ]
+})
+export class SharedModule { }
