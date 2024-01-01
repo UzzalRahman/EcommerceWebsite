04a6090f7906bf1590cff9cd469827ded456c83f
Uzzal Rahman <uzzal.ewu95@gmail.com>
2024-01-01 21:22:37 +0600

    bug fix

--- Client/src/app/shop/shop.module.ts
+++ Client/src/app/shop/shop.module.ts
@@ -5,6 +5,7 @@ import { ProductItemComponent } from './product-item/product-item.component';
 import { SharedModule } from '../shared/shared.module';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { RouterModule } from '@angular/router';
+import { ShopRoutingModule } from './shop-routing.module';
 
 
 
@@ -19,6 +20,6 @@ import { RouterModule } from '@angular/router';
     SharedModule,
     RouterModule
   ],
-  exports: [ShopComponent]
+  exports: [ShopRoutingModule]
 })
 export class ShopModule { }
